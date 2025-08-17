fb8947e533c27cad928d6d91a5f31510e5797df8
Hust06 <hust22061989@gmail.com>
2025-08-17 18:34:50 +0000

    docs: added Ukrainian comments for clarity and learning

--- contracts/FHECounter.sol
+++ contracts/FHECounter.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.24;
 import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
+//Контракл перемінної, що працює з зашифрованими даними
 /// @title A simple FHE counter contract
 contract FHECounter is SepoliaConfig {
     euint32 private _count;
 
+    //Отримання поточного значення перемінної
     /// @notice Returns the current count
     function getCount() external view returns (euint32) {
         return _count;
     }
 
+    //Збільшення перемінної на зашифроване значення
     /// @notice Increments the counter by a specified encrypted value.
     /// @dev This example omits overflow/underflow checks for simplicity and readability.
     /// In a production contract, proper range checks should be implemented.
@@ -25,6 +28,7 @@ contract FHECounter is SepoliaConfig {
         FHE.allow(_count, msg.sender);
     }
 
+    //Зменьшення перемінної на зашифроване значення
     /// @notice Decrements the counter by a specified encrypted value.
     /// @dev This example omits overflow/underflow checks for simplicity and readability.
     /// In a production contract, proper range checks should be implemented.
